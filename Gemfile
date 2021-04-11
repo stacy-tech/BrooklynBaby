86cf78747dbc0a7cea3419e7f87ae1a7dda9b9e3
stacy <stacydn23@gmail.com>
2021-04-11 01:54:02 -0400

    build navbar for app

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'dotenv-rails'
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.5'
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,31 @@
 class UsersController < ApplicationController
-   def index
-     @user = User.find_by_id(params[:id])
-   end
+  def index
+    @user = User.find_by_id(params[:id])
+  end
 
-   def new
+  def new
     @user = User.new
-   end
+  end
       
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        session[:user_id] = @user.id 
             redirect_to @user
     else
-        flash[:error]=@user.errors.full_messages.to_sentence
-        render :new
-   end
+      flash[:error]=@user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
 
-   def show
+  def show
     redirect_if_not_logged_in
     @user = User.find_by_id(params[:id])
     redirect_to '/' if !@user
-   end
+  end
 
    private
-   def user_params
+  def user_params
     params.require(:user).permit(:name, :email, :password)
-   end
+  end
 end
