beb56d92e83884340befe1ad7e63cc5dd5e36861
stacy <stacydn23@gmail.com>
2021-04-13 22:54:18 -0400

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Homepage
   root 'welcome#home'
 
+  # signup route
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
  
+  # login route
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+
+  # logout route
+  delete '/logout', to: 'sessions#destroy'
 
  #  get 'auth/facebook', to: 'sessions#facebook'
 
